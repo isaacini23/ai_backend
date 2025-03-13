f73b69370cd1e18acba617f9aff4648a27f4b2ee
isaacini123 <iniisaac2@gmail.com>
2025-03-13 16:29:56 +0100

    newest changes

--- api/models.py
+++ api/models.py
@@ -8,3 +8,12 @@ class Transcription(models.Model):
 
     def __str__(self):
         return f"Transcription {self.id} - {self.created_at}"
+
+class Transcription1(models.Model):
+    audio_file = models.FileField(upload_to='audio/')
+    image_file = models.ImageField(upload_to='images/', blank=True, null=True)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Transcription {self.id} - {self.created_at}"
